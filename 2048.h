f766d380082f1cce19a42bd6280dbbc189c6ca2e
Beber91 <33326134+Beber91@users.noreply.github.com>
2017-12-13 17:01:51 +0100

    Add files via upload
    avec la structure jeu ET ncurses

--- 2048.h
+++ 2048.h
@@ -12,6 +12,10 @@ using namespace std;
 const int GAUCHE = 4, DROITE = 6, HAUT = 8, BAS = 2;
 typedef vector<vector<int>> Plateau;
 
+struct jeu {    //définit une structure jeu contenant un plateau ainsi qu'un score
+    Plateau p;
+    int s;
+};
 
 Plateau plateauVide();
 Plateau plateauInitial();
@@ -21,19 +25,19 @@ Plateau suppression0Bas(Plateau plateau);
 Plateau suppression0Gauche(Plateau plateau);
 Plateau suppression0Droite(Plateau plateau);
 
-Plateau additionHaut(Plateau plateau);
-Plateau additionBas(Plateau plateau);
-Plateau additionGauche(Plateau plateau);
-Plateau additionDroite(Plateau plateau);
+jeu additionHaut(jeu game);
+jeu additionBas(jeu game);
+jeu additionGauche(jeu game);
+jeu additionDroite(jeu game);
 
-Plateau deplacementHaut(Plateau plateau);
-Plateau deplacementBas(Plateau plateau);
-Plateau deplacementGauche(Plateau plateau);
-Plateau deplacementDroite(Plateau plateau);
-Plateau deplacement(Plateau plateau, int direction);
+jeu deplacementHaut(jeu game);
+jeu deplacementBas(jeu game);
+jeu deplacementGauche(jeu game);
+jeu deplacementDroite(jeu game);
+jeu deplacement(jeu game, int direction);
 
 string dessine(Plateau g);
-bool estTermine(Plateau plateau);
+bool estTermine(jeu game);
 bool estGagnant(Plateau plateau);
 int score(Plateau plateau);
 
